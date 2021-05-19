6077eebbe77637bfebc51d6548814a6211638ba9
AndrejKovacik57 <xkovacika1@stuba.sk>
2021-05-19 20:31:46 +0200

    GET /product get all products

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/Product/Product.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/Product/Product.java
@@ -1,11 +1,16 @@
 package sk.stuba.fei.uim.oop.assignment3.Product;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/sk/stuba/fei/uim/oop/assignment3/Product/ProductService.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/Product/ProductService.java
@@ -3,12 +3,26 @@ package sk.stuba.fei.uim.oop.assignment3.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService implements IProductService{
 
-    @Autowired
+
     private ProductRepository repository;
+    @Autowired
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+        Product p1 = new Product();
+        p1.setUnit("procesor");
+        this.repository.save(p1);
+        Product p2 = new Product();
+        p2.setUnit("graficka karta");
+        this.repository.save(p2);
+    }
 
-
-
+    @Override
+    public List<Product> getAll() {
+        return this.repository.findAll();
+    }
 }
